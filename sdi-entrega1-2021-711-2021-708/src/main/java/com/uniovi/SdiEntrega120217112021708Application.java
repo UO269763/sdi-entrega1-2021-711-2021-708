59c24b81877e4c9fa398c8ed0c39a72060e75863
UO269763 <UO269763@uniovi.es>
2021-03-10 23:52:11 +0100

    Primer commit

---
+++ sdi-entrega1-2021-711-2021-708/src/main/java/com/uniovi/SdiEntrega120217112021708Application.java
@@ -0,0 +1,13 @@
+package com.uniovi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdiEntrega120217112021708Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdiEntrega120217112021708Application.class, args);
+	}
+
+}
