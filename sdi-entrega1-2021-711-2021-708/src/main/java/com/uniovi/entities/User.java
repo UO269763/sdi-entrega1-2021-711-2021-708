39c4083aa766b33d9b75a65fe0b2ee06b919305b
UO269763 <UO269763@uniovi.es>
2021-03-11 00:58:45 +0100

    Hecha la primera parte de registro de usuario.

---
+++ sdi-entrega1-2021-711-2021-708/src/main/java/com/uniovi/entities/User.java
@@ -0,0 +1,98 @@
+package com.uniovi.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
+@Entity
+@Table(name = "user")
+public class User {
+	
+	@Id
+	@GeneratedValue
+	private long id;
+	@Column(unique = true)
+	private String email;
+	private String name;
+	private String lastName;
+	private double dinero;
+	
+	private String password;
+
+	@Transient // propiedad que no se almacena e la tabla.
+	private String passwordConfirm;
+	
+	public User(String email, String name, String lastName) {
+		super();
+		this.email = email;
+		this.name = name;
+		this.lastName = lastName;
+		this.dinero=100;
+	}
+
+	public User() {
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getPasswordConfirm() {
+		return passwordConfirm;
+	}
+
+	public void setPasswordConfirm(String passwordConfirm) {
+		this.passwordConfirm = passwordConfirm;
+	}
+	
+	public void setDinero(double dinero) {
+		this.dinero=dinero;
+	}
+	
+	public double getDinero() {
+		return dinero;
+	}
+	
+	
+
+
+}
