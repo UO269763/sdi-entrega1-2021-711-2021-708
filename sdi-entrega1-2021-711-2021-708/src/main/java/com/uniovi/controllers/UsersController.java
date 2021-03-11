e6099e6dd6091555d90c18efd64ad36c0fb2f1d9
uo265578 <uo265578@uniovi.es>
2021-03-11 17:35:19 +0100

    Inicio de sesión, inicio internacionalizacion y errores

--- sdi-entrega1-2021-711-2021-708/src/main/java/com/uniovi/controllers/UsersController.java
+++ sdi-entrega1-2021-711-2021-708/src/main/java/com/uniovi/controllers/UsersController.java
@@ -10,34 +10,59 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.uniovi.entities.User;
+import com.uniovi.services.SecurityService;
 import com.uniovi.services.UsersService;
+import com.uniovi.validators.SignUpFormValidator;
 
 @Controller
 public class UsersController {
-	
+
 	@Autowired
 	private UsersService usersService;
+
+	@Autowired
+	private SecurityService securityService;
+	
+	@Autowired
+	private SignUpFormValidator signUpFormValidator;
 
 	@RequestMapping(value = "/signup", method = RequestMethod.GET)
 	public String signup(Model model) {
 		model.addAttribute("user", new User());
 		return "signup";
 	}
-	
+
 	@RequestMapping(value = "/signup", method = RequestMethod.POST)
-	public String signup(@ModelAttribute @Validated User user, BindingResult result, Model model) {
+	public String signup(@Validated User user, BindingResult result) {
+		signUpFormValidator.validate(user, result);
+		if(result.hasErrors()) {
+			return "signup";
+		}
 		usersService.addUser(user);
+		securityService.autoLogin(user.getEmail(), user.getPasswordConfirm());
 		return "redirect:home";
 	}
-	
+
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
 	public String login(Model model) {
 		return "login";
 	}
-	
+
 	@RequestMapping(value = { "/home" }, method = RequestMethod.GET)
 	public String home(Model model) {
 		return "home";
+	}
+
+	@RequestMapping(value = "/user/add")
+	public String getUser(Model model) {
+		model.addAttribute("usersList", usersService.getUsers());
+		return "user/add";
+	}
+
+	@RequestMapping(value = "/user/add", method = RequestMethod.POST)
+	public String setUser(@ModelAttribute User user) {
+		usersService.addUser(user);
+		return "redirect:/user/list";
 	}
 
 }
